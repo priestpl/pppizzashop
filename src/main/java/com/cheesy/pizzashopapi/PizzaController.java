30e57df1e09e6455a452b075596e8544a45e687b
Priest, Paula <priestp@nationwide.com>
2018-10-17 11:37:52 -0400

    First commit

---
+++ src/main/java/com/cheesy/pizzashopapi/PizzaController.java
@@ -0,0 +1,13 @@
+package com.cheesy.pizzashopapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PizzaController {
+
+    @RequestMapping("/pizzas")
+    public String getPizzas() {
+        return "Pizza is good";
+    }
+}
---
+++ src/main/java/com/cheesy/pizzashopapi/PizzaShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.cheesy.pizzashopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShopApiApplication.class, args);
+	}
+}
